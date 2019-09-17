da363fb63314e8f0dfee910bc464cf0943d2f60c
Youhei Nozaki <youhey.hey0505@gmail.com>
2019-09-17 15:32:45 +0900

    create operation logs

--- src/reducers/operationLogs.js
+++ src/reducers/operationLogs.js
@@ -1,11 +1,11 @@
 import {
-  ADD_OPERATION_LOGS,
+  ADD_OPERATION_LOG,
   DELETE_ALL_OPERATION_LOG
 } from '../actions'
 
 const operationLogs = (state = [], action) => {
   switch (action.type) {
-    case ADD_OPERATION_LOGS:
+    case ADD_OPERATION_LOG:
       const operationLog ={
         description: action.description,
         operatedAt: action.operatedAt
